38eb2e821500e20ddd90d2757334f052b93babe6
ashraf-git <aliashraf.hena@gmail.com>
2020-03-13 11:43:42 +0600

    Create Component, return multiple HTML from a component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>I am react Lover</h1>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+function Person(){
+  const personStyle = {
+    border: '1px solid green',
+    margin: '10px',
+    padding: '4px'
+  }
+  return(
+    <div style = {personStyle}>
+      <h1>Name: Sakib Al Hasan</h1>
+      <h3>Hero of the Year</h3>
+    </div>
+  ) 
 }
 
 export default App;
