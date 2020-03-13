8ead6aeac286e887ca12b64589241a17eda27423
ashraf-git <aliashraf.hena@gmail.com>
2020-03-13 22:08:30 +0600

    Product Component, send dynamic data by props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header></Header>
+        <Shop></Shop>
       </header>
     </div>
   );
