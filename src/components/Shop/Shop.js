fc12d10572b7b6dd7b66bc52fa8edaadfd6edb17
ashraf-git <aliashraf.hena@gmail.com>
2020-03-14 11:44:18 +0600

    Pass eventhandler to child component and handle event

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,11 +5,17 @@ import Product from '../Product/Product';
 const Shop = () => {
     const first10 = fakeData.slice(0,15);
     const [products, setProducts] = useState(first10);
+    const handleAddProduct = (product) => {
+        console.log("product added", product)
+    }
        return (
         <div className="shop-container">
             <div className="product-container">
                 {
-                    products.map(prod => <Product product={prod}></Product>)
+                    products.map(prod => <Product
+                        handleAddProduct = {handleAddProduct}
+                         product={prod}
+                         ></Product>)
                 }
             </div>
             <div className="cart-container">
